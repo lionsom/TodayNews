7170514fbfac0454c11ee94fe5ed856da179088e
lionsom <lionsom_lin@qq.com>
2018-10-24 10:48:25 +0800

    copy

--- TodayNews/TodayNews/Main/Feature/ForthMain/Study/Study_copy_mutableCopy/Study_copy_mutableCopy.m
+++ TodayNews/TodayNews/Main/Feature/ForthMain/Study/Study_copy_mutableCopy/Study_copy_mutableCopy.m
@@ -18,19 +18,75 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    self.view.backgroundColor = LXRandomColor;
+    self.view.backgroundColor = [UIColor whiteColor];
 
-#pragma mark -- NSString
-    // 字符串
-    [self NSStringCopyAndMutableCopy];
+// =======================
+    //创建segmentControl 分段控件
+    UISegmentedControl *segC = [[UISegmentedControl alloc]initWithFrame:CGRectMake(0,50,SCREEN_WIDTH,50)];
+    //添加小按钮
+    [segC insertSegmentWithTitle:@"NSString的copy与mutableCopy" atIndex:0 animated:YES];
+    [segC insertSegmentWithTitle:@"NSArray的copy与mutableCopy" atIndex:1 animated:YES];
+    //设置样式
+    [segC setTintColor:[UIColor grayColor]];
+    //设置字体样式
+    [segC setTitleTextAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:10],NSForegroundColorAttributeName:[UIColor redColor]} forState:UIControlStateNormal];
+    //添加事件
+    [segC addTarget:self action:@selector(segmentValueChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:segC];
     
+// =======================
+    //创建segmentControl 分段控件
+    UISegmentedControl *segC1 = [[UISegmentedControl alloc]initWithFrame:CGRectMake(0,150,SCREEN_WIDTH,50)];
+    //添加小按钮
+    [segC1 insertSegmentWithTitle:@"容器浅拷贝" atIndex:0 animated:YES];
+    [segC1 insertSegmentWithTitle:@"单层深拷贝" atIndex:1 animated:YES];
+    [segC1 insertSegmentWithTitle:@"双层深拷贝" atIndex:2 animated:YES];
+    [segC1 insertSegmentWithTitle:@"完全深拷贝:归档解档" atIndex:3 animated:YES];
+    [segC1 insertSegmentWithTitle:@"完全深拷贝:系统自带方法" atIndex:4 animated:YES];
+    //设置样式
+    [segC1 setTintColor:[UIColor grayColor]];
+    //设置字体样式
+    [segC1 setTitleTextAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:8],NSForegroundColorAttributeName:[UIColor redColor]} forState:UIControlStateNormal];
+    //添加事件
+    [segC1 addTarget:self action:@selector(segmentValueChanged1:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:segC1];
 }
+
+
+//点击按钮事件
+-(void)segmentValueChanged:(UISegmentedControl *)seg{
+    NSUInteger segIndex = [seg selectedSegmentIndex];
+    //将当旧VC的view移除，然后在添加新VC的view
+    if (segIndex == 0) {
+        [self NSStringCopyAndMutableCopy];
+    }else if (segIndex == 1){
+        [self NSArrayCopyAndMutableCopy];
+    }else{
+    }
+}
+
+-(void)segmentValueChanged1:(UISegmentedControl *)seg{
+    NSUInteger segIndex = [seg selectedSegmentIndex];
+    //将当旧VC的view移除，然后在添加新VC的view
+    if (segIndex == 0) {
+        [self shallowCopy];
+    }else if (segIndex == 1){
+        [self one_deep_copy];
+    }else if (segIndex == 2){
+        [self two_deep_copy];
+    }else if (segIndex == 3){
+        [self all_depp_copy_1];
+    }else if (segIndex == 4){
+        [self all_deep_copy_2];
+    }else{
+    }
+}
+
 
 
 // #pragma mark - NSString、NSMutableString、NSArray、NSMutableArray分别进行copy和mutableCopy时的情况
 
 #pragma mark - 1.系统的非容器类对象
-// 这里指的是NSString,NSNumber等等一类的对象。
 
 -(void)NSStringCopyAndMutableCopy {
     // const是常量字符串,存在常量区
@@ -63,18 +119,163 @@
     [originStrMutableCopy appendString:@"origin"];
 
 #pragma warnning - ERROR
-    [mutableOriginStrCopy appendString:@"mm"];   // ERROR
+    //[mutableOriginStrCopy appendString:@"mm"];   // ERROR
 }
 
 
 #pragma mark - 2.系统的容器类对象
 // 指NSArray，NSDictionary等。
+-(void)NSArrayCopyAndMutableCopy {
+    NSArray * arr = [NSArray arrayWithObjects:
+                     [NSMutableString stringWithString:@"one"],
+                     [NSMutableString stringWithString:@"two"],
+                     [NSMutableString stringWithString:@"three"],
+                     [NSMutableString stringWithString:@"four"],nil];
+    NSArray * arrcopy = [arr copy];
+    NSMutableArray * arrmutablecopy = [arr mutableCopy];
+    NSLog(@"arr = %p = %p",arr,arr[0]);
+    NSLog(@"arrcopy = %p = %p",arrcopy,arrcopy[0]);
+    NSLog(@"arrmutablecopy = %p = %p",arrmutablecopy,arrmutablecopy[0]);
+
+    NSMutableString * mStr;
+    mStr = arr[0];
+    [mStr appendString:@"--array"];
+
+    NSLog(@"改变内部元素后 arr：%@ = %p",arr,arr[0]);
+    NSLog(@"改变内部元素后 arrcopy：%@ = %p",arrcopy,arrcopy[0]);
+    NSLog(@"改变内部元素后 arrmutablecopy：%@ = %p",arrmutablecopy,arrmutablecopy[0]);
+
+
+    
+    NSMutableArray *mutableArr = [NSMutableArray arrayWithObjects:
+                                  [NSMutableString stringWithString:@"abc"],
+                                  [NSMutableString stringWithString:@"def"],
+                                  [NSMutableString stringWithString:@"ghi"],
+                                  [NSMutableString stringWithString:@"jkl"], nil];
+    NSArray * mutableArrcopy = [mutableArr copy];
+    NSMutableArray * mutableArrmutablecopy = [mutableArr mutableCopy];
+    NSLog(@"mutableArr = %p = %p",mutableArr,mutableArr[0]);
+    NSLog(@"mutableArrcopy = %p = %p",mutableArrcopy,mutableArrcopy[0]);
+    NSLog(@"mutableArrmutablecopy = %p = %p",mutableArrmutablecopy,mutableArrmutablecopy[0]);
+    
+    NSMutableString * mStr1;
+    mStr1 = mutableArr[0];
+    [mStr1 appendString:@"--mutablearray"];
+    
+    [mutableArrmutablecopy addObject:@"FFF"];
+    
+    NSLog(@"改变内部元素后 mutableArr：%@ = %p",mutableArr,mutableArr[0]);
+    NSLog(@"改变内部元素后 mutableArrcopy：%@ = %p",mutableArrcopy,mutableArrcopy[0]);
+    NSLog(@"改变内部元素后 mutableArrmutablecopy：%@ = %p",mutableArrmutablecopy,mutableArrmutablecopy[0]);
+}
 
 
 
+#pragma mark - 容器 浅copy
+
+-(void)shallowCopy {
+    // 和NSString浅copy的验证步骤一样
+    NSArray *arr = [NSArray arrayWithObjects:@"1", nil];
+    NSArray *copyArr = [arr copy];
+    
+    NSLog(@"%p", arr);
+    NSLog(@"%p", copyArr);
+}
+
+#pragma mark - 容器 单层深拷贝
+
+-(void)one_deep_copy {
+    NSArray *arr = [NSArray arrayWithObjects:@"1", nil];
+    NSArray *copyArr = [arr mutableCopy];
+    
+    NSLog(@"%p", arr);
+    NSLog(@"%p", copyArr);
+    
+    // 打印arr、copyArr内部元素进行对比
+    NSLog(@"%p", arr[0]);
+    NSLog(@"%p", copyArr[0]);
+}
 
 
+#pragma mark - 容器 双层深copy
+// 这里的双层指的是完成了NSArray对象和NSArray容器内对象的深copy（为什么不说完全，是因为无法处理NSArray中还有一个NSArray这种情况）。
+-(void)two_deep_copy {
+    // 随意创建一个NSMutableString对象
+    NSMutableString *mutableString = [NSMutableString stringWithString:@"1"];
+    // 随意创建一个包涵NSMutableString的NSMutableArray对象
+    NSMutableString *mutalbeString1 = [NSMutableString stringWithString:@"1"];
+    NSMutableArray *mutableArr = [NSMutableArray arrayWithObjects:mutalbeString1, nil];
+    // 将mutableString和mutableArr放入一个新的NSArray中
+    NSArray *testArr = [NSArray arrayWithObjects:mutableString, mutableArr, nil];
+    // 通过官方文档提供的方式创建copy
+    NSArray *testArrCopy = [[NSArray alloc] initWithArray:testArr copyItems:YES];
+    
+    // testArr和testArrCopy指针对比
+    NSLog(@"%p", testArr);
+    NSLog(@"%p", testArrCopy);
+    
+    // testArr和testArrCopy中元素指针对比
+    // mutableString对比
+    NSLog(@"%p", testArr[0]);
+    NSLog(@"%p", testArrCopy[0]);
+    // mutableArr对比
+    NSLog(@"%p", testArr[1]);
+    NSLog(@"%p", testArrCopy[1]);
+    
+    // mutableArr中的元素对比，即mutalbeString1对比
+    NSLog(@"%p", testArr[1][0]);
+    NSLog(@"%p", testArrCopy[1][0]);
+}
 
+
+#pragma mark - 容器 完全深copy
+
+-(void)all_depp_copy_1 {
+    // 随意创建一个NSMutableString对象
+    NSMutableString *mutableString = [NSMutableString stringWithString:@"1"];
+    // 随意创建一个包涵NSMutableString的NSMutableArray对象
+    NSMutableString *mutalbeString1 = [NSMutableString stringWithString:@"1"];
+    NSMutableArray *mutableArr = [NSMutableArray arrayWithObjects:mutalbeString1, nil];
+    // 将mutableString和mutableArr放入一个新的NSArray中
+    NSArray *testArr = [NSArray arrayWithObjects:mutableString, mutableArr, nil];
+    // 通过归档、解档方式创建copy
+    NSArray *testArrCopy = [NSKeyedUnarchiver unarchiveObjectWithData:
+                            [NSKeyedArchiver archivedDataWithRootObject:testArr]];;
+    
+    // testArr和testArrCopy指针对比
+    NSLog(@"%p", testArr);
+    NSLog(@"%p", testArrCopy);
+    
+    // testArr和testArrCopy中元素指针对比
+    // mutableString对比
+    NSLog(@"%p", testArr[0]);
+    NSLog(@"%p", testArrCopy[0]);
+    // mutableArr对比
+    NSLog(@"%p", testArr[1]);
+    NSLog(@"%p", testArrCopy[1]);
+    
+    // mutableArr中的元素对比，即mutalbeString1对比
+    NSLog(@"%p", testArr[1][0]);
+    NSLog(@"%p", testArrCopy[1][0]);
+}
+
+
+-(void)all_deep_copy_2 {
+    NSMutableArray *marry1 = [[NSMutableArray alloc] init];
+    
+    NSMutableString *mstr1 = [[NSMutableString alloc]initWithString:@"value1"];
+    NSMutableString *mstr2 = [[NSMutableString alloc]initWithString:@"value2"];
+    
+    [marry1 addObject:mstr1];
+    [marry1 addObject:mstr2];
+    
+    NSArray *marray2 = [[NSArray alloc] initWithArray:marry1 copyItems:YES];
+    
+    NSLog(@"marry1:%p - %@ \r\n",marry1,marry1);
+    NSLog(@"marry2:%p - %@ \r\n",marray2,marray2);
+    NSLog(@"数组元素地址:value1:%p - value2:%p \r\n",marry1[0],marry1[1]);
+    NSLog(@"数组元素地址:value1:%p - value2:%p \r\n",marray2[0],marray2[1]);
+}
 
 
 
